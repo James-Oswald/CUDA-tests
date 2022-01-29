a4d0294079ecc1136920e586d7a86253ff07b5e8
James Oswald <jtoswald@albany.edu>
2022-01-29 00:34:48 -0500

    made easier to read

--- src/sumThrustVectors.cu
+++ src/sumThrustVectors.cu
@@ -9,16 +9,18 @@ __global__ void sumRow(double* vecStart, double* vec2Start){
 double sum(const std::vector<double>& vec){
     thrust::device_vector<double> dv(vec.begin(), vec.end());
     while(dv.size() > 1){
-        bool odd = dv.size() % 2 == 1;
+        int newSize = dv.size()/2+(dv.size() % 2 == 1);
         double* dvloc = dv.data().get();
-        sumRow<<<dv.size()/2, 1>>>(dvloc, dvloc+(dv.size()/2+(odd?1:0)));
+        sumRow<<<dv.size()/2, 1>>>(dvloc, dvloc+newSize);
         cudaDeviceSynchronize();
-        dv.resize(dv.size()/2+(odd?1:0));
+        dv.resize(newSize);
     }
     return dv[0];
 }
 
 int main(){
-    std::vector<double> data = {.5, .5, .5, .5, .9};
+    std::vector<double> data;
+    for(int i = 0; i < 100; i++)
+        data.push_back(0.5);
     std::cout<<sum(data)<<std::endl;
 }
